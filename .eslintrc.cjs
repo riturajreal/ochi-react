6b965edb1eb151b4c1eb36a54be7986d8a1949d8
Rituraj Jha <riturajreal01@gmail.com>
2024-02-08 18:44:18 +0530

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 2020, sourceType: 'module' },
+  settings: { react: { version: '17.0' } }, // Adjust the version as per your React version
+  plugins: ['react-refresh'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
