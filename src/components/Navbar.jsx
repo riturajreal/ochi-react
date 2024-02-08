cd24665a51c845d70fd7c011d2d7b82e4981a076
Rituraj Jha <riturajreal01@gmail.com>
2024-02-08 18:45:02 +0530

    completed nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,9 +15,9 @@ const Navbar = () => {
         </div>
 
         <div className="links flex gap-10">
-         {["Services", "Our work", "About us", "Insights", "Contact"].map((item, index) => (
+            {["Services", "Our work", "About us", "Insights", "Contact"].map((item, index) => (
             <a key={index} className={`text-lg capitalize font-light ${index === 4 && "ml-32" }`} >{item}</a>
-         ))}
+            ))}
         </div>
 
 
